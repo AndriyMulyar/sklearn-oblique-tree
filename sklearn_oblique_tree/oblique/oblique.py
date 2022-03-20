2c9aca7c9d86d378cf538b0a99fea4f8e2226d33
TbabmBarry <71264455+TbabmBarry@users.noreply.github.com>
2022-03-20 23:43:24 +0100

    feat: update python interface function description

--- sklearn_oblique_tree/oblique/oblique.py
+++ sklearn_oblique_tree/oblique/oblique.py
@@ -8,7 +8,6 @@ class ObliqueTree(BaseEstimator, ClassifierMixin):
 
     def __init__(self, splitter="oc1, axis_parallel", number_of_restarts=20, max_perturbations=5, random_state=1):
         """
-
         :param splitter: 'oc1' for stochastic hill climbing, 'cart' for CART multivariate, 'axis_parallel' for traditional.
         'oc1, axis_parallel' will also consider axis parallel splits when computing best oblique split. Setting 'cart' overrides other options.
         :param number_of_restarts: number of times to restart in effort to escape local minimums
@@ -19,8 +18,6 @@ class ObliqueTree(BaseEstimator, ClassifierMixin):
         self.splitter = splitter
         self.number_of_restarts = number_of_restarts
         self.max_perturbations = max_perturbations
-
-
 
     def fit(self, X, y):
         """
@@ -35,19 +32,39 @@ class ObliqueTree(BaseEstimator, ClassifierMixin):
         self.tree = Tree(splitter = self.splitter)
         self.tree.fit(X,y, random_state, self.splitter, self.number_of_restarts, self.max_perturbations)
 
-
-
     def predict(self, X):
+        """
+        Makes an instance prediction
+        :param X: a 2d numpy array of attributes
+        :return: an integer label
+        """
         return self.tree.predict(X)
     
     def treeDepth(self):
+        """
+        Return fitted tree depth
+        :return: an integer count
+        """
         return self.tree.treeDepth()
 
     def leafCount(self):
+        """
+        Return the number of fitted tree leaf nodes
+        :return: an integer count
+        """
         return self.tree.leafCount()
 
     def nodeCount(self):
+        """
+        Return the number of fitted tree internal nodes
+        :return: an integer count
+        """
         return self.tree.nodeCount()
 
     def getCoef(self, attr_num):
+        """
+        Return an array of pre ordered coefficients for each node
+        :param attr_num: an integer representing the number of attributes
+        :return: an array of floats
+        """
         return self.tree.getCoef(attr_num)
